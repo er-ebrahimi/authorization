d54ff4cff02003e9585de506651060c3745e84eb
Erfan Ebrahimi <74706761+er-ebrahimi@users.noreply.github.com>
2025-09-05 13:50:54 +0330

    Fix middleware to redirect by itself

--- src/hooks/use-login.ts
+++ src/hooks/use-login.ts
@@ -1,12 +1,10 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { loginApi } from "@/services/auth/login-service";
 import { useAuth } from "@/contexts/auth-context";
-import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 export const useLogin = () => {
   const { login } = useAuth();
-  const router = useRouter();
   const queryClient = useQueryClient();
 
   return useMutation({
@@ -14,8 +12,8 @@ export const useLogin = () => {
     onSuccess: (data) => {
       login(data.token, data.user);
       queryClient.invalidateQueries({ queryKey: ["user"] });
-      router.push("/");
       toast.success("Successfully logged in!");
+      window.location.reload();
     },
     onError: (error) => {
       console.error("Login failed:", error);
